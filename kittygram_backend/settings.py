90a6d9c4e6953c5d8eb9b515fa9997403fabd34d
Maksim Arkhipov <manjanfe@gmail.com>
2023-03-07 19:20:58 +0700

    Save for write documentation.

--- kittygram_backend/settings.py
+++ kittygram_backend/settings.py
@@ -106,7 +106,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
 }
